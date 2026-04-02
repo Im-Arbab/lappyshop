e295f9d7432722d86b364de50c075b2b3bbdc76d
Your Name <your@email.com>
2026-04-02 20:32:25 +0530

    fix: resolve uploadImage conflict

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -8,10 +8,6 @@ try{
 
 res.json({
 success:true,
-<<<<<<< HEAD
-// url:`http://localhost:8080/uploads/${req.file.filename}`
-=======
->>>>>>> d83c4a18126415be5be21b2cf2c52ddf67e35a74
 url:`${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
 })
 
