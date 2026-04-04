cd8dd64c67383e7ed6b6d771d15d30249ef27484
Your Name <your@email.com>
2026-04-04 21:27:48 +0530

    fix: razorpay env variable

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,7 +1,7 @@
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_SECRET,
 });
 
