51cb224dccd6cf9203346b2d57f9c040308250d6
Your Name <your@email.com>
2026-04-14 22:01:20 +0530

    fix:filter

--- controller/product/filterProduct.js
+++ controller/product/filterProduct.js
@@ -1,68 +1,56 @@
-// const productModel = require("../../models/productModel")
 
-// const filterProductController = async(req,res)=>{
-//  try{
-//         const categoryList = req?.body?.category || []
-
-//         const product = await productModel.find({
-//             category :  {
-//                 "$in" : categoryList
-//             }
-//         })
-
-//         res.json({
-//             data : product,
-//             message : "product",
-//             error : false,
-//             success : true
-//         })
-//  }catch(err){
-//     res.json({
-//         message : err.message || err,
-//         error : true,
-//         success : false
-//     })
-//  }
-// }
-
-
-// module.exports = filterProductController
 const productModel = require("../../models/productModel");
 
 const filterProduct = async (req, res) => {
   try {
-
-    const { category, brand, ram, processor } = req.body;
+    const {
+      category,
+      brand,
+      ram,
+      processor,
+      generation,
+      storage,
+      screenSize,
+      price
+    } = req.body;
 
     let filter = {};
 
-    /* CATEGORY */
-    if (category) {
-      filter.category = category;
-    }
+    if (category) filter.category = category;
 
-    /* BRAND */
     if (brand) {
-      filter.brand = new RegExp(`^${brand}$`, "i"); // case insensitive
+      filter.brand = new RegExp(`^${brand}$`, "i");
     }
 
-    /* RAM FILTER (variants) */
     if (ram) {
-      filter["variants.ram"] = new RegExp(`^${ram}$`, "i");
+      filter.$or = [
+        { ram: new RegExp(ram, "i") },
+        { "variants.ram": new RegExp(ram, "i") }
+      ];
     }
 
-    /* PROCESSOR FILTER (specifications) */
     if (processor) {
-      filter["specifications.performance.processor"] =
-        new RegExp(processor, "i");
+      filter.$or = [
+        { processor: new RegExp(processor, "i") },
+        { "specifications.performance.processor": new RegExp(processor, "i") }
+      ];
+    }
+
+    if (generation) filter.generation = generation;
+
+    if (storage) {
+      filter.storage = new RegExp(storage, "i");
+    }
+
+    if (screenSize) filter.screenSize = screenSize;
+
+    if (price) {
+      filter["variants.price"] = { $lte: Number(price) };
     }
 
     const products = await productModel.find(filter);
 
-    res.json({
-      success: true,
-      data: products,
-    });
+    res.json({ success: true, data: products });
 
   } catch (error) {
     res.status(500).json({
