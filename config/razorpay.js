accbe85cda3772a2cea9e60b1e9ea949ea5e8f32
Your Name <your@email.com>
2026-04-15 13:18:12 +0530

    fix:razorpay

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,10 +1,10 @@
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY,
-  key_secret: process.env.RAZORPAY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
-console.log("RAZOR CONFIG:", process.env.RAZORPAY_KEY, process.env.RAZORPAY_SECRET);
+console.log("RAZOR CONFIG:", process.env.RAZORPAY_KEY_ID, process.env.RAZORPAY_KEY_SECRET);
 
 module.exports = razorpay;
 
